ca77383b2277a93ae672a682c45fa4ba6c59481d
roman015 <roman015@iamcrazy.in>
2015-01-12 00:25:45 +0530

    Simple falling and spawning
    Basic Falling and Spawning scripts. Game is still not in playable state

---
+++ Tetris/Assets/ShapeComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeComponent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("fallOneStep", 1.0f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void fallOneStep(){
+		GetComponent<Transform> ().position += Vector3.down;
+	}
+}
---
+++ Tetris/Assets/ShapeSpawnerComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeSpawnerComponent : MonoBehaviour {
+
+	public GameObject[] pieces = new GameObject[5];
+
+	// Use this for initialization
+	void Start () {
+		spwanPiece ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void spwanPiece(){
+		int select = Random.Range (0, 3);
+
+		Debug.Log ("Spwaning piece : " + pieces [select].name);
+		Instantiate (pieces [select], transform.position, Quaternion.identity);
+	}
+}
